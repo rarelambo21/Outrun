f90821db81aa26df8d483d647978b7758597682d
rarelambo21 <35502394+rarelambo21@users.noreply.github.com>
2021-03-26 15:29:48 +0300

    error

--- Outrun/Outrun.cpp
+++ Outrun/Outrun.cpp
@@ -7,6 +7,25 @@ using namespace sf;
 
 int width = 1024;
 int height = 768;
+int roadW = 2000;
+int segL = 200;	// segment length
+float camD = 0.84;	// camera depth
+
+struct Line {
+	float x, y, z;	// 3d center of line 
+	float X, Y, W;	// screen coord
+	float scale;
+
+	Line() { x = y = z = 0; }
+
+	// from world to screen coorinates
+	void project(int camX, int camY, int camZ) {
+		scale = camD / (z - camZ);
+		X = (1 + scale * (x - camX)) * width / 2;
+		Y = (1 - scale * (y - camY)) * height / 2;
+		W = scale * roadW * width / 2;
+	}
+};
 
 void drawQuad(RenderWindow &w, Color c, int x1, int y1, int w1, int x2, int y2, int w2) {
 	ConvexShape shape(4);
@@ -22,6 +41,17 @@ int main () {
 	RenderWindow app(VideoMode(width, height), "Outrun Racing!");
 	app.setFramerateLimit(144);
 
+	std::vector<Line> lines;
+
+	for (int i = 0; i < 1600; i++) {
+		Line line;
+		line.z = i * segL;
+
+		lines.push_back(line);
+	}
+
+	int N = lines.size();
+
 	while (app.isOpen()) {
 		Event e;
 
@@ -31,7 +61,24 @@ int main () {
 		}
 
 		app.clear();
-		drawQuad(app, Color::Green, 500, 500, 200, 500, 300, 100);
+		
+		////// draw road //////
+
+ 		for (int n = 0; n < 300; n++) {
+			Line& l = lines[n % N];
+			l.project(0, 1500, 0);
+
+			Color grass =  (n / 3) % 2?Color(16, 200, 16) : Color(0, 154, 0);
+			Color rumble = (n / 3) % 2?Color(245, 255, 255) : Color(0, 0, 0);
+			Color road =   (n / 3) % 2?Color(107, 107, 107) : Color(105, 105, 105);
+
+			Line p = lines[(n - 1) % N];	// previous line 
+
+			drawQuad (app, grass, 0, p.Y, width, 0, l.Y, width);
+			drawQuad (app, rumble, p.X, p.Y, p.W * 1.2, l.X, l.Y, l.W * 1.2);
+			drawQuad (app, road, p.X, p.Y, p.W, l.X, l.Y, l.W);
+		}
+
 		app.display();
 	}
 
