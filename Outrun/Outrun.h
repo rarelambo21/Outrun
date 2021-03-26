9e9125f23356fbad08becd090d77bcf40347df91
rarelambo21 <35502394+rarelambo21@users.noreply.github.com>
2021-03-26 17:05:58 +0300

    working out errors

--- Outrun/Outrun.h
+++ Outrun/Outrun.h
@@ -5,6 +5,23 @@ public:
 	Outrun();
 	virtual ~Outrun();
 
+	void drawQuad(RenderWindow &w, Color c, int x1, int y1, int w1, int x2, int y2, int w2);
+
+	struct Line {
+		float x, y, z; //3d center of line
+		float X, Y, W; //screen coord
+		float curve, spriteX, clip, scale;
+		Sprite sprite;
+
+		Line(){
+			spriteX = curve = x = y = z = 0;
+		}
+
+		void project(int camX, int camY, int camZ);
+		void drawSprite(RenderWindow& app);
+
+	};
+
 	int playgame();
 
 private:
